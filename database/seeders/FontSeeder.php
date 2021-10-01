a32a9a96d48f5ce85d249dedf37adabe2714b6e3
Ajeya Paul <ajeya@blueberrywebs.com>
2021-10-01 20:32:57 +0530

    laravel

---
+++ database/seeders/FontSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class FontSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
